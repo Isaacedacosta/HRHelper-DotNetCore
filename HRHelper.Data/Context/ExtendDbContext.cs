34557513ae8c86e4b9b9db06c1c6bd68ff5b8101
Isaac <isaacdacosta@hotmail.com>
2022-04-01 15:15:58 -0300

    Add Entity Framework - Configure ConnectionString

---
+++ HRHelper.Data/Context/ExtendDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRHelper.Domain.Entities;
+using HRHelper.Data.Mappings;
+
+namespace HRHelper.Data.Context
+{
+    public class ExtendDbContext : DbContext
+    {
+        public ExtendDbContext(DbContextOptions<ExtendDbContext> options) : base(options) { }
+
+
+        #region DBSets
+        public DbSet<User> Users { get; set; } 
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- HRHelper/Startup.cs
+++ HRHelper/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HRHelper.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRHelper
 {
@@ -21,6 +23,9 @@ namespace HRHelper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ExtendDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("HRHelper_Local")).EnableSensitiveDataLogging());
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
