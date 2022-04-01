e71e148a7ca42fd26b0e179b015366a031888fca
Isaac <isaacdacosta@hotmail.com>
2022-04-01 16:20:34 -0300

    Add Repository IRepository Entity - Edit User

--- HRHelper.Data/Migrations/20220401191549_Add Repository  IRepository.Designer.cs
+++ HRHelper.Data/Migrations/20220401191549_Add Repository  IRepository.Designer.cs
@@ -4,14 +4,16 @@ using HRHelper.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HRHelper.Data.Migrations
 {
     [DbContext(typeof(ExtendDbContext))]
-    partial class ExtendDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220401191549_Add Repository  IRepository")]
+    partial class AddRepositoryIRepository
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- HRHelper.Domain/Entities/User.cs
+++ HRHelper.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRHelper.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace HRHelper.Domain.Entities
 {
-    public class User
+    public class User : Entity
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
